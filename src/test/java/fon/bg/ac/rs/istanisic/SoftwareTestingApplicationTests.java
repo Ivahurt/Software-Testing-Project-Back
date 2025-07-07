f0e87673a2a6d1248616da28131e50a344709856
Iva Stanisic <iva.hurt574@gmail.com>
2025-07-07 18:02:59 +0200

    Initial project setup

---
+++ src/test/java/fon/bg/ac/rs/istanisic/SoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.bg.ac.rs.istanisic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
