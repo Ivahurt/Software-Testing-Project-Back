df476caaf080a36da5596dfb91ad98999dbfa900
Iva Stanisic <iva.hurt574@gmail.com>
2025-08-24 00:02:12 +0200

    Added new function in GlobalExceptionHandler class

--- src/main/java/fon/bg/ac/rs/istanisic/exception/GlobalExceptionHandler.java
+++ src/main/java/fon/bg/ac/rs/istanisic/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package fon.bg.ac.rs.istanisic.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.http.ResponseEntity;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -39,5 +43,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(500).body(Map.of(
                 "message", "Došlo je do greške: " + ex.getMessage()
         ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> fieldErrors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Došlo je do greške: Validation failed for argument");
+        response.put("errors", fieldErrors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
