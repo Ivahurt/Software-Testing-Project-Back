2ed001406e4132918a3d6f741735338295a0b7a3
Iva Stanisic <iva.hurt574@gmail.com>
2025-09-24 15:38:21 +0100

    Implemented forbid direct sum of payments insert

--- src/main/resources/liquibase/postgresql/setup/operations/fnc_forbid_direct_sum_of_payments.sql
+++ src/main/resources/liquibase/postgresql/setup/operations/fnc_forbid_direct_sum_of_payments.sql
@@ -1,14 +1,10 @@
 create or replace function forbid_direct_sum_of_payments()
 returns trigger as $$
 begin
-    if tg_op = 'insert' and new.sum_of_payments is not null and new.sum_of_payments <> 0 then
-        raise exception 'Direktan unos ukupne isplate za osobu nije dozvoljen';
-    end if;
+   if new.sum_of_payments is distinct from 0 then
+    raise EXCEPTION 'Direktan unos ukupne isplate nije dozvoljen';
+   end if;
 
-    if tg_op = 'update' and new.sum_of_payments is distinct from old.sum_of_payments then
-        raise exception 'Direktna izmena ukupne isplate za osobu nije dozvoljena';
-    end if;
-
-    return new;
+   return new;
 end;
 $$ language plpgsql;
