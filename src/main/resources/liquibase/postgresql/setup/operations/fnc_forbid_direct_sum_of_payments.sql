74458c5b7a182f342284a67c1874f1b568ed8a9c
Iva Stanisic <iva.hurt574@gmail.com>
2025-09-26 15:15:11 +0100

    Improved trigger for sum_of_payment in person table

--- src/main/resources/liquibase/postgresql/setup/operations/fnc_forbid_direct_sum_of_payments.sql
+++ src/main/resources/liquibase/postgresql/setup/operations/fnc_forbid_direct_sum_of_payments.sql
@@ -1,8 +1,12 @@
 create or replace function forbid_direct_sum_of_payments()
 returns trigger as $$
 begin
-   if new.sum_of_payments is distinct from 0 then
-    raise EXCEPTION 'Direktan unos ukupne isplate nije dozvoljen';
+   if TG_OP = 'INSERT' and NEW.sum_of_payments is distinct from 0 then
+      raise EXCEPTION 'Direktan unos ukupne sume za isplatu nije dozvoljen';
+   end if;
+
+   if TG_OP = 'UPDATE' and NEW.sum_of_payments is distinct from OLD.sum_of_payments then
+      raise EXCEPTION 'Direktna izmena ukupne sume za isplatu nije dozvoljena';
    end if;
 
    return new;
