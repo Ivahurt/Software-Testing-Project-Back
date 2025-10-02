b61ee855f147b872f2010f0719e116a945c25835
Iva Stanisic <iva.hurt574@gmail.com>
2025-10-02 16:13:07 +0100

    Added insert to person table

--- src/main/java/fon/bg/ac/rs/istanisic/service/PersonService.java
+++ src/main/java/fon/bg/ac/rs/istanisic/service/PersonService.java
@@ -131,12 +131,14 @@ public class PersonService {
     }
 
     public PersonDTO getPersonSumPayments(Long uniqueId) throws Exception{
+        System.out.println("Usao je u fju getPersonSumPayments u PersonService klasi");
         Optional<Person> foundPerson = personRepository.findByUniqueIdentificationNumber(uniqueId);
         if (foundPerson.isEmpty()) {
             throw new Exception("Osoba ne postoji");
         }
 
         Person person = foundPerson.get();
+        System.out.println("Pronadjena osoba: " + person);
         return personConverter.toDto(person);
     }
 }
--- src/main/resources/liquibase/postgresql/setup/dml/insert_to_person.sql
+++ src/main/resources/liquibase/postgresql/setup/dml/insert_to_person.sql
@@ -3,4 +3,5 @@ INSERT INTO person(first_name, last_name,
 VALUES
  ('Iva','Ivic','2001-01-28', 2801001717186,3,1),
  ('Pera','Peric','2000-01-10', 2001001715180,1,2),
- ('Aleksandar', 'Aleksandric', '1999-12-12', 1920199951769, 2,2);
+ ('Aleksandar', 'Aleksandric', '1999-12-12', 1920199951769, 2,2),
+ ('Nikola', 'Nikolić', '1970-11-11', 1920194871769, 3,3);
