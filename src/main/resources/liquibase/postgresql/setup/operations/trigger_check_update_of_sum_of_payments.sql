221327a4a9382790be223cd0b8ca070685ac7429
Iva Stanisic <iva.hurt574@gmail.com>
2025-10-02 13:08:18 +0100

    Working version of project

--- src/main/resources/liquibase/postgresql/setup/operations/trigger_check_update_of_sum_of_payments.sql
+++ src/main/resources/liquibase/postgresql/setup/operations/trigger_check_update_of_sum_of_payments.sql
@@ -1,3 +1,14 @@
+create or replace function protect_sum_of_payments()
+returns trigger as $$
+begin
+    if current_setting('app.bypass_sum_of_payments', true) = 'on' then
+        return new;
+    end if;
+
+    raise exception 'Direct update of sum_of_payments is not allowed';
+end;
+$$ language plpgsql;
+
 create trigger trg_protect_sum_of_payments
 before insert or update of sum_of_payments on person
 for each row
